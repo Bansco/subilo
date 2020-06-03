988582d44639ebfce7ab60322395d8118203421a
gillchristian <gillchristiang@gmail.com>
2020-06-03 22:25:22 +0200

    feat(webhook): Use GitHub push webhook payload
    Filter repo & run commands

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,108 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 use std::fs;
+use std::process::{Command, Output, Stdio};
+use std::str;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
+struct GitHubUser {
+    login: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Repository {
+    name: String,
+    owner: GitHubUser,
+}
+
+// https://developer.github.com/webhooks/event-payloads/#push
+#[derive(Debug, Deserialize, Serialize)]
+struct PushEvent {
+    #[serde(rename(serialize = "ref", deserialize = "ref"))]
+    ref_: String,
+    repository: Repository,
+    sender: GitHubUser,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 struct Project {
+    // TODO: add branch (pattern?) to filter on
+    // TODO: add env option
     username: String,
     repository: String,
     path: String,
     commands: Vec<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct Config {
+    // TODO: add port to run on
     projects: Vec<Project>,
 }
 
-#[get("/")]
-async fn index() -> impl Responder {
-    let contents =
-        fs::read_to_string("./config.toml").expect("Something went wrong reading the file");
+// TODO: handle failure by returning Result
+fn run_command(path: &String, command: &String) -> Output {
+    Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .stdout(Stdio::piped())
+        .current_dir(path)
+        .env_clear()
+        .spawn()
+        .expect("failed to execute child")
+        .wait_with_output()
+        .expect("failed to wait on child")
+}
 
-    let config: Config = toml::from_str(&contents).unwrap();
+fn run_project(project: &Project) -> String {
+    let mut result: String = "".to_owned();
 
-    println!("{:#?}", config);
+    for command in &project.commands {
+        let path = shellexpand::tilde(&project.path).into_owned();
 
-    format!("{}", contents)
+        let output = run_command(&path, &command);
+
+        result.push_str(&format!("$ {}\n", command));
+
+        result.push_str(&format!("{}\n", str::from_utf8(&output.stdout).unwrap()));
+
+        if !output.status.success() {
+            result.push_str(&match output.status.code() {
+                Some(code) => format!("Exit {}", code),
+                None => format!("Process terminated by signal"),
+            });
+            break;
+        }
+    }
+
+    result
 }
 
 #[post("/webhook")]
-async fn webhook() -> impl Responder {
-    HttpResponse::Ok()
+async fn webhook(info: web::Json<PushEvent>) -> impl Responder {
+    // TODO: 400? 500? on missing Threshfile ?
+    // TODO: where should Threshfile be by default ? 
+    // TODO: accept flag for Threshfile location
+    let contents =
+        fs::read_to_string("./Threshfile").expect("Something went wrong reading the file");
+
+    let config: Config = toml::from_str(&contents).unwrap();
+
+    let maybe_project = config.projects.iter().find(|x| {
+        x.username == info.repository.owner.login && x.repository == info.repository.name
+    });
+
+    match maybe_project {
+        // TODO: respond right away and run 'job' asynchronously after
+        // TODO: write logs to file instead
+        Some(project) => HttpResponse::Ok().body(run_project(&project)),
+        None => HttpResponse::NotFound().body("404 Not Found"),
+    }
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-        HttpServer::new(|| App::new().service(index).service(webhook))
+    HttpServer::new(|| App::new().service(webhook))
         .bind("127.0.0.1:9000")?
         .run()
         .await
@@ -47,12 +115,9 @@ mod test {
 
     #[actix_rt::test]
     async fn test_webhook_ok() {
-        let mut server = test::init_service(App::new().service(index).service(webhook)).await;
+        let mut server = test::init_service(App::new().service(webhook)).await;
 
-        let payload = &[
-            ("name", "tigrin"),
-            ("city", "amsterdam"),
-        ];
+        let payload = &[("name", "tigrin"), ("city", "amsterdam")];
         let req = test::TestRequest::post()
             .uri("/webhook")
             .set_json(payload)
