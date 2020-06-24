66c38f47d6d06936812027e3b3cc4707f294b14e
ndelvalle <nicolas.delvalle@gmail.com>
2020-06-24 01:52:34 -0300

    Add add_to_path fn

--- install.sh
+++ install.sh
@@ -3,11 +3,16 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# DOWNLOAD_URL="https://github.com/Huemul/thresh/releases/download/alpha_v4/thresh-x86-64-linux"
-DOWNLOAD_URL="https://github.com/moneditas/api/releases/download/latest/moneditas-x86-64-linux"
+DOWNLOAD_URL="https://github.com/Huemul/thresh/releases/download/alpha_v4/thresh-x86-64-linux"
 
 default_install_dir() {
   [ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.thresh" || printf %s "${XDG_CONFIG_HOME}/thresh"
+}
+
+add_to_path () {
+  if ! echo "$PATH" | /bin/grep -Eq "(^|:)$1($|:)" ; then
+    PATH="$PATH:$1"
+  fi
 }
 
 main() {
@@ -19,6 +24,9 @@ main() {
   test -d "$_dir" || mkdir "$_dir"
   curl -L --show-error $DOWNLOAD_URL --output "$_file"  
   chmod +x "$_file"
+
+  # TODO: Make this work
+  add_to_path _dir
 }
 
 main
