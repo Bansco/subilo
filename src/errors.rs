d87328f74c7956472aa475f2ff13d2d45304a576
Nicolas Del Valle <nicolas.delvalle@gmail.com>
2020-07-01 17:11:32 -0300

    Improve error handling (#42)
    * Error handling initial implementation
    
    * Update src/main.rs
    
    Co-authored-by: Christian Gill <gillchristiang@gmail.com>
    
    * Cargo fmt
    
    * Improve error handling
    
    * Update Error names
    
    Co-authored-by: Christian Gill <gillchristiang@gmail.com>

---
+++ src/errors.rs
@@ -0,0 +1,34 @@
+use actix_http::ResponseBuilder;
+use actix_web::http::{header, StatusCode};
+use actix_web::HttpResponse;
+
+#[derive(thiserror::Error, Debug)]
+#[error("...")]
+pub enum ThreshError {
+    #[error("Failed to read Thresh file, {}", source)]
+    ReadThreshFile { source: std::io::Error },
+
+    #[error("Failed to create log directory, {}", source)]
+    CreateLogDir { source: std::io::Error },
+
+    #[error("Failed to create log file, {}", source)]
+    CreateLogFile { source: std::io::Error },
+
+    #[error("Failed to write log file, {}", source)]
+    WriteLogFile { source: std::io::Error },
+
+    #[error("Failed to parse Thresh file, {}", source)]
+    ParseThreshFile { source: toml::de::Error },
+}
+
+impl actix_web::error::ResponseError for ThreshError {
+    fn error_response(&self) -> HttpResponse {
+        ResponseBuilder::new(self.status_code())
+            .set_header(header::CONTENT_TYPE, "text/html; charset=utf-8")
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
