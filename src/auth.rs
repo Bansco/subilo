b290f19d7ccd715a0efe848403b4e369e76af031
ndelvalle <nicolas.delvalle@gmail.com>
2020-07-02 20:52:46 -0300

    Add more error handling

--- src/auth.rs
+++ src/auth.rs
@@ -1,11 +1,11 @@
 use actix_web::dev::ServiceRequest;
-use actix_web::Error;
 use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
 use actix_web_httpauth::extractors::AuthenticationError;
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 
 use super::Context;
+use super::SubiloError;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
@@ -14,7 +14,7 @@ struct Claims {
     exp: usize,
 }
 
-pub fn create_token(secret: &str) -> Result<String, jsonwebtoken::errors::Error> {
+pub fn create_token(secret: &str) -> Result<String, SubiloError> {
     let claims = Claims {
         sub: "subilo:agent".to_owned(),
         company: "subilo".to_owned(),
@@ -29,13 +29,14 @@ pub fn create_token(secret: &str) -> Result<String, jsonwebtoken::errors::Error>
         &header,
         &claims,
         &EncodingKey::from_secret(secret.as_bytes()),
-    )
+    ).map_err(|err| SubiloError::Authenticate { source: err })
 }
 
+// TODO: Migrate result to SubiloError and handle app data context unwrap.
 pub async fn validator(
     req: ServiceRequest,
     credentials: BearerAuth,
-) -> Result<ServiceRequest, Error> {
+) -> Result<ServiceRequest, actix_web::Error> {
     let context = req.app_data::<Context>().unwrap();
 
     let config = req
--- src/errors.rs
+++ src/errors.rs
@@ -20,6 +20,9 @@ pub enum SubiloError {
     #[error("Failed to write log file, {}", source)]
     WriteLogFile { source: std::io::Error },
 
+    #[error("Failed to clone log file, {}", source)]
+    CloneLogFile { source: std::io::Error },
+
     #[error("Failed to read file name")]
     ReadFileName {},
 
@@ -28,6 +31,9 @@ pub enum SubiloError {
 
     #[error("Failed to execute command with child process, {}", source)]
     ExecuteCommand { source: std::io::Error },
+
+    #[error("Failed to authenticate request, {}", source)]
+    Authenticate { source: jsonwebtoken::errors::Error },
 }
 
 impl actix_web::error::ResponseError for SubiloError {
