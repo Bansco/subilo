397237782bad96ace13545ee09b6ca1d632f9b7d
ndelvalle <nicolas.delvalle@gmail.com>
2020-07-31 21:04:17 -0300

    Clean up database actor

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,7 @@
+use crate::job;
 use actix::prelude::*;
 use rusqlite::NO_PARAMS;
-use rusqlite::{params, Connection, Result};
+use rusqlite::{Connection, Result};
 
 pub struct Database {
     connection: rusqlite::Connection,
@@ -8,8 +9,13 @@ pub struct Database {
 
 impl Database {
     pub fn new(path: &str) -> Self {
-        let connection = Connection::open(path).expect("Failed to connecet to the local database");
+        let connection = Connection::open(path).expect("Failed to connecet to the database");
         Self { connection }
+    }
+
+    fn create_tables(&self) -> Result<usize> {
+        self.connection
+            .execute(job::CREATE_JOB_TABLE_QUERY, NO_PARAMS)
     }
 }
 
@@ -17,70 +23,27 @@ impl Actor for Database {
     type Context = Context<Self>;
 
     fn started(&mut self, _ctx: &mut Context<Self>) {
-        let query = "
-            CREATE TABLE IF NOT EXISTS jobs (
-                id TEXT PRIMARY KEY NOT NULL,
-                name TEXT NOT NULL,
-                status TEXT NOT NULL,
-                started_at TEXT NOT NULL,
-                ended_at TEXT
-            )
-        ";
-        self.connection.execute(query, NO_PARAMS).unwrap();
+        debug!("Connected to database");
+        self.create_tables().unwrap();
     }
 
     fn stopped(&mut self, _ctx: &mut Context<Self>) {
-        debug!("Actor is stopped");
+        debug!("Disconnected from database");
     }
 }
 
 #[derive(Message)]
-#[rtype(result = "()")]
-pub struct NewJob {
-    pub id: String,
-    pub name: String,
-    pub started_at: String,
+#[rtype(result = "Result<usize>")]
+pub struct Execute {
+    pub query: String,
+    pub params: Vec<String>,
 }
 
-impl Handler<NewJob> for Database {
-    type Result = ();
+impl Handler<Execute> for Database {
+    type Result = Result<usize>;
 
-    fn handle(&mut self, job: NewJob, _ctx: &mut Context<Self>) {
-        let query = "
-            INSERT INTO jobs (id, name, status, started_at)
-            VALUES (?1, ?2, ?3, ?4)
-        ";
-        let params = params![job.id, job.name, "started".to_owned(), job.started_at];
-
-        match self.connection.execute(query, params) {
-            Ok(_) => debug!("Job inserted successfully"),
-            Err(err) => error!("Failed to insert job {}", err),
-        }
-    }
-}
-
-#[derive(Message)]
-#[rtype(result = "()")]
-pub struct UpdateJob {
-    pub id: String,
-    pub status: String,
-    pub ended_at: String,
-}
-
-impl Handler<UpdateJob> for Database {
-    type Result = ();
-
-    fn handle(&mut self, job: UpdateJob, _ctx: &mut Context<Self>) {
-        let query = "
-            UPDATE jobs
-            SET status = ?2, ended_at = ?3
-            WHERE id = ?1
-        ";
-        let params = params![job.id, job.status, job.ended_at];
-
-        match self.connection.execute(query, params) {
-            Ok(_) => debug!("Job updated successfully"),
-            Err(err) => error!("Failed to update job {}", err),
-        }
+    fn handle(&mut self, execute: Execute, _ctx: &mut Context<Self>) -> Result<usize> {
+        self.connection
+            .execute(execute.query.as_str(), execute.params)
     }
 }
--- src/job.rs
+++ src/job.rs
@@ -2,10 +2,30 @@ use crate::core;
 use crate::database;
 use crate::Context;
 use crate::SubiloError;
-use chrono::Utc;
 use nanoid::nanoid;
 use std::fs;
 use std::io::Write;
+
+pub const CREATE_JOB_TABLE_QUERY: &str = "
+    CREATE TABLE IF NOT EXISTS jobs (
+        id TEXT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL,
+        status TEXT NOT NULL,
+        started_at TEXT NOT NULL,
+        ended_at TEXT
+    )
+";
+
+pub const INSERT_JOB_QUERY: &str = "
+    INSERT INTO jobs (id, name, status, started_at)
+    VALUES (?1, ?2, ?3, ?4)
+";
+
+pub const UPDATE_JOB_QUERY: &str = "
+    UPDATE jobs
+    SET status = ?2, ended_at = ?3
+    WHERE id = ?1
+";
 
 pub struct Witness {
     id: String,
@@ -23,18 +43,18 @@ impl Witness {
         fs::create_dir_all(&context.logs_dir)
             .map_err(|err| SubiloError::CreateLogDir { source: err })?;
 
-        let mut log =
-            fs::File::create(job_name).map_err(|err| SubiloError::CreateLogFile { source: err })?;
+        let mut log = fs::File::create(&job_name)
+            .map_err(|err| SubiloError::CreateLogFile { source: err })?;
 
         log.write_all(&project.description().as_bytes())
             .map_err(|err| SubiloError::WriteLogFile { source: err })?;
 
         let id = nanoid!();
+        let started_at = now();
 
-        context.database.do_send(database::NewJob {
-            id: id.clone(),
-            name: "test".to_owned(),
-            started_at: Utc::now().to_rfc3339(),
+        context.database.do_send(database::Execute {
+            query: INSERT_JOB_QUERY.to_owned(),
+            params: vec![id.clone(), job_name, started_at],
         });
 
         Ok(Self { id, context, log })
@@ -43,16 +63,15 @@ impl Witness {
     pub fn report_command(&mut self, command: &str) -> Result<(), SubiloError> {
         self.log
             .write_all(format!("$ {}\n", &command).as_bytes())
-            .map_err(|err| SubiloError::WriteLogFile { source: err })?;
-
-        Ok(())
+            .map_err(|err| SubiloError::WriteLogFile { source: err })
     }
 
     pub fn report_command_success(&self) -> Result<(), SubiloError> {
-        self.context.database.do_send(database::UpdateJob {
-            id: self.id.clone(),
-            status: "succeeded".to_owned(),
-            ended_at: Utc::now().to_rfc3339(),
+        let ended_at = now();
+        let status = "succeeded".to_owned();
+        self.context.database.do_send(database::Execute {
+            query: UPDATE_JOB_QUERY.to_owned(),
+            params: vec![self.id.clone(), status, ended_at],
         });
 
         Ok(())
@@ -73,10 +92,11 @@ impl Witness {
                 .map_err(|err| SubiloError::WriteLogFile { source: err })?,
         };
 
-        self.context.database.do_send(database::UpdateJob {
-            id: self.id.clone(),
-            status: "failed".to_owned(),
-            ended_at: Utc::now().to_rfc3339(),
+        let ended_at = now();
+        let status = "failed".to_owned();
+        self.context.database.do_send(database::Execute {
+            query: UPDATE_JOB_QUERY.to_owned(),
+            params: vec![self.id.clone(), status, ended_at],
         });
 
         Ok(())
@@ -87,18 +107,21 @@ impl Witness {
             .write_all(err.to_string().as_bytes())
             .map_err(|err| SubiloError::WriteLogFile { source: err })?;
 
-        self.context.database.do_send(database::UpdateJob {
-            id: self.id.clone(),
-            status: "failed".to_owned(),
-            ended_at: Utc::now().to_rfc3339(),
+        let ended_at = now();
+        let status = "failed".to_owned();
+        self.context.database.do_send(database::Execute {
+            query: UPDATE_JOB_QUERY.to_owned(),
+            params: vec![self.id.clone(), status, ended_at],
         });
 
         Ok(())
     }
 
     pub fn try_clone_log(&self) -> Result<std::fs::File, std::io::Error> {
-        let log = self.log.try_clone()?;
-
-        Ok(log)
+        self.log.try_clone()
     }
+}
+
+fn now() -> String {
+    chrono::Utc::now().to_rfc3339_opts(chrono::SecondsFormat::Millis, true)
 }
